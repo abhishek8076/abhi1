08a9137f9bb7f6fc7a2e47db91b2894c1a706961
jhaabhishek8076 <jhaabhishek8076@gmail.com>
2022-09-08 11:41:11 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React,{useState,useEffect} from "react";
+import ResponsiveAppBa from './components/NavBar/navbar';
+import About from './components/About/About';
+import Footer from "./components/Footer_/Footer";
+import ResumeNew from "./components/resume/Resume";
+// import BackToTop from './components/BackToTop';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+// import ScrollToTop from "./components/ScrollToTop";
+// import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Preloader load={load} /> */}
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <ResponsiveAppBa />
+        {/* <ScrollToTop /> */}
+        <About/>
+        <ResumeNew />
+        <Routes>
+          {/* <Route path="/" element={<Abhishek />} /> */}
+          {/* <Route path="/project" element={<Projects />} /> */}
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<ResumeNew />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/About/About.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+const About=()=>{
+    return(
+    <h1 style={{textAlign:"center",color:"-moz-initial"}}>About</h1>
+    )
+}
+export default About;
